50d09ef808aeacf3bb9445a5d1ae6d54db2a0a23
Maccarrone Alessia <mcclss01m52d960x@studium.unict.it>
2022-06-29 21:12:13 +0200

    update project

--- progetto/State/StateFile.java
+++ progetto/State/StateFile.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 // RUOLO: State
 public interface StateFile {
-    public void read();         //per leggere i file modificati
-    public StateFile write(String word, String path) throws IOException;   //per scrivere in un secondo file evidenziando le parole trovate
-	public void show();         //mostra lo stato della ricerca
+    public void read(); // per leggere i file modificati
+
+    public StateFile write(String word, String path) throws IOException; // per scrivere in un secondo file evidenziando
+                                                                         // le parole trovate
+
+    public void show(); // mostra lo stato della ricerca
 }
