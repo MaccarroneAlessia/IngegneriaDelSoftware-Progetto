431e0531e3bbc607e8fa8f68ef4e320da03af700
Maccarrone Alessia <mcclss01m52d960x@studium.unict.it>
2022-06-29 21:09:03 +0200

    update project

--- progetto/State/Folder.java
+++ progetto/State/Folder.java
@@ -3,57 +3,54 @@ package org.progetto.State;
 import java.io.File;
 import java.util.List;
 import java.util.stream.IntStream;
-import java.util.Objects;
 
-// la cartella è una classe di supporto che fa interfaccia al client e il sottosistema -> come un Facade
+// la cartella è una classe di supporto che fa da interfaccia al client-> come un Facade
 public class Folder {
-	static private int m; 						// numero file di testo
-	static private File winFolder;				// file caltella
-	private boolean find[];						//se true abbiamo trovato delle parole
-												//in quel corrispondente file di indice i
-	private List<MyFile> theFile; 				//si salva in una lista gli oggetti MyFile
+	static private int m; // numero file di testo
+	static private File winFolder; // file caltella
+	private boolean find[]; // se true abbiamo trovato delle parole
+							// in quel corrispondente file di indice i
+	private List<MyFile> theFile; // si salva in una lista gli oggetti MyFile
 
-    public Folder(File path, final String[] toSearch){
+	public Folder(File path, final String[] toSearch) {
 		String name = path.toString();
 		boolean errore = getArrayFile(name, toSearch);
 
 		System.out.println("Cartella : " + winFolder + "  con " + m + " file di testo");
-	
-		if(!errore)
+
+		if (!errore)
 			System.out.println("An error has occurred");
 
-		IntStream.rangeClosed(1,m).forEach(i -> find[i] = false);
-    }
+		IntStream.rangeClosed(1, m).forEach(i -> find[i] = false);
+	}
 
-	private boolean getArrayFile(String name, final String[] toSearch){
+	private boolean getArrayFile(String name, final String[] toSearch) {
 		String ROOT = "C:";
 		name = name.replace("\\", "/"); // Create a new variable
 		char FS = File.separatorChar;
-		
-		try{
+
+		try {
 			winFolder = new File(ROOT + FS + name);
 			m = (int) winFolder.length();
 			String[] supp = winFolder.list();
 			int n = toSearch.length;
-			IntStream.rangeClosed(0,m-1).forEach(i -> theFile.add(new MyFile(supp[i], toSearch, n)));
+			IntStream.rangeClosed(0, m - 1).forEach(i -> theFile.add(new MyFile(supp[i], toSearch, n)));
 			return true;
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
 		}
-		catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
 
 		return false;
 	}
 
-	//metodi che chiamano i metodi di MyFile e che verranno chiamati dal Client per la ricerca
-	public void search(){
+	// metodi che chiamano i metodi di MyFile e che verranno chiamati dal Client per
+	// la ricerca
+	public void search() {
 		System.out.println("Stiamo cercando nei file della cartella le parole passate...");
-		IntStream.rangeClosed(0,m-1).forEach(i -> theFile.get(i).searcher());
+		IntStream.rangeClosed(0, m - 1).forEach(i -> theFile.get(i).searcher());
 
 		System.out.println("Mostriamo i file modificati");
-		IntStream.rangeClosed(0,m-1).forEach(i -> theFile.get(i).show());
+		IntStream.rangeClosed(0, m - 1).forEach(i -> theFile.get(i).show());
 	}
-	
-
 
 }
