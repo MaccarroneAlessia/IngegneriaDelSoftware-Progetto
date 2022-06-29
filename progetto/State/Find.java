8ea65680f177f628a2499e7f4527fd6149d5e4ba
Maccarrone Alessia <mcclss01m52d960x@studium.unict.it>
2022-06-29 21:05:54 +0200

    update project

--- progetto/State/Find.java
+++ progetto/State/Find.java
@@ -1,6 +1,5 @@
 package org.progetto.State;
 
-import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,27 +8,26 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Find implements StateFile{
+public class Find implements StateFile {
 
     private String pathmod;
     private ArrayList<String> wordsFind = new ArrayList<>();
 
-    private String newFile(String txt){
+    private String setpathmode(String txt) {
         String path2 = "C:/mod" + txt;
 
         try {
             File file = new File(path2);
-            if (file.exists()){
+            if (file.exists()) {
                 System.out.println("Il file " + path2 + " esiste già");
                 double n = Math.random();
-                String path3 = n + txt;
+                String path3 = "C:/mod" + n + txt;
                 return newFile(path3);
             }
-            if (file.createNewFile()){
+            if (file.createNewFile()) {
                 System.out.println("Il file è stato creato: " + path2);
                 return path2;
-            }
-            else{
+            } else {
                 System.out.println("Il file non può essere creato");
                 return newFile(path2);
             }
@@ -39,11 +37,11 @@ public class Find implements StateFile{
         }
     }
 
-    private boolean highlight(){
+    private boolean highlight() {
 
-        FileInputStream fstream = null;
-        DataInputStream in = null;
-        BufferedWriter out = null;
+        FileInputStream fstream;
+        DataInputStream in;
+        BufferedWriter out;
 
         try {
             // apro il file
@@ -51,53 +49,53 @@ public class Find implements StateFile{
 
             // prendo l'inputStream
             in = new DataInputStream(fstream);
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String strLine;
-            StringBuilder fileContent = new StringBuilder();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+                String strLine;
+                StringBuilder fileContent = new StringBuilder();
 
-            // Leggo il file riga per riga
-            while ((strLine = br.readLine()) != null) {
+                // Leggo il file riga per riga
+                while ((strLine = br.readLine()) != null) {
 
-                int n = wordsFind.size();
-                
-                if(strLine.contains(wordsFind.get(n-1))){        //cerca l'ultima parola inserita nell'array delle parole trovate
-                    // se la riga è uguale a quella ricercata
-                    strLine.replace(wordsFind.get(n-1),"***"+wordsFind+"***");
-                    fileContent.append(strLine + System.getProperty("line.separator"));
-                    
-                } else {
-                    // ... altrimenti la trascrivo così com'è
-                    fileContent.append(strLine);
-                    fileContent.append(System.getProperty("line.separator"));
+                    int n = wordsFind.size();
+
+                    if (strLine.contains(wordsFind.get(n - 1))) { // cerca l'ultima parola inserita nell'array delle
+                                                                  // parole trovate
+                        // se la riga è uguale a quella ricercata
+                        String str = strLine.replace(wordsFind.get(n - 1), "***" + wordsFind + "***");
+                        fileContent.append(str + System.getProperty("line.separator"));
+
+                    } else {
+                        // ... altrimenti la trascrivo così com'è
+                        fileContent.append(strLine + System.getProperty("line.separator"));
+                        // fileContent.append(System.getProperty("line.separator"));
+                    }
                 }
+
+                // Sovrascrivo il file con il nuovo contenuto (aggiornato)
+                FileWriter fstreamWrite = new FileWriter(pathmod);
+                out = new BufferedWriter(fstreamWrite);
+                out.write(fileContent.toString());
             }
 
-            // Sovrascrivo il file con il nuovo contenuto (aggiornato)
-            FileWriter fstreamWrite = new FileWriter(pathmod);
-            out = new BufferedWriter(fstreamWrite);
-            out.write(fileContent.toString());
-
-        } finally {
-            // chiusura dell'output e dell'input
-            try {
-                fstream.close();
-                out.flush();
-                out.close();
-                in.close();
-                return true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-        }catch (IOException e) {
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try { // chiusura dell'output e dell'input
+            fstream.close();
+            out.flush();
+            out.close();
+            in.close();
+            return true;
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
-
     }
 
-    private boolean modify(String txt) throws IOException{
-        pathmod = newFile(txt);             //Creare un nuovo file txt da modificare
+    private boolean modify(String txt) throws IOException {
+        pathmod = setpathmode(txt); // Creare un nuovo file txt da modificare
         FileWriter writer = new FileWriter(pathmod, true);
         PrintWriter out = new PrintWriter(writer);
 
@@ -105,10 +103,8 @@ public class Find implements StateFile{
             File ftxt = new File(txt);
             File fpathmod = new File(pathmod);
             copyFile(ftxt, fpathmod);
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
-            return false;
         }
         out.close();
 
@@ -123,26 +119,21 @@ public class Find implements StateFile{
     public void read() {
         Path path = Paths.get(pathmod);
         try {
-            //String content = Files.readString(path);
-            //System.out.println(content);
-            
             Stream<String> lines = Files.lines(path);
             String content = lines.collect(Collectors.joining(System.lineSeparator()));
             System.out.println(content);
             lines.close();
-            
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
     }
 
     @Override
     public StateFile write(String word, String path) throws IOException {
         wordsFind.add(word);
-
         boolean control = this.modify(path);
-        if(control) {
+        if (control) {
             System.out.println("file modificato correttamente");
             return this;
         }
@@ -158,5 +149,4 @@ public class Find implements StateFile{
     }
 
 }
-
 
